72cbeb8681cfccadc85539e61602c5ab4a678532
Thiago Mateo <Tmateo2005@hotmail.com>
2022-09-29 12:09:20 -0300

    Cambios minimos

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import mongoose from 'mongoose';
 
 
 const PORT = process.env.PORT || 3000;
-const DB = proces.env.DB ||
+//const DB = proces.env.DB ||
 
 const app = express();
 
-mongoose.connect(DB)
-  .then(() => { console.log('Se conecto exitosamente') })
-  .catch((err) => { console.err(err) });
+// mongoose.connect(DB)
+//   .then(() => { console.log('Se conecto exitosamente') })
+//   .catch((err) => { console.err(err) });
 
 app.use(morgan('dev'));
 app.use(express.json());
